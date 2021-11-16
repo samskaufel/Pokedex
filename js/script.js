1195c4f9522cd2e781e6f3305f1dab00913fd6e4
samskaufel <85969886+samskaufel@users.noreply.github.com>
2021-11-16 13:02:04 -0800

    set up initial structure of each page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// browser displays "Hello World" in a pop-up window when "index.html" file is opened
+alert('Hello World');
+
+// this prints "pho" on "index.html" 
+let favoriteFood = 'pho'; 
+document.write(favoriteFood);
