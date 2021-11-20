c28fd339c557c8d739f34b1cfc0b5a55bc37c9cc
samskaufel <85969886+samskaufel@users.noreply.github.com>
2021-11-19 23:19:32 -0800

    added "addListItem", added button styling, and added event listener

--- js/script.js
+++ js/script.js
@@ -1,40 +1,74 @@
 // IIFE
 let pokemonRepository = (function () {
-  let pokemonList = [
+    let pokemonList = [
     {
-      name: 'Bulbasaur',
-      height: 0.7,
-      type: ['grass', 'poison'],
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass', 'poison'],
     },
     {
-      name: 'Ivysaur',
-      height: 1,
-      type: ['grass', 'poison'],
+    name: 'Ivysaur',
+    height: 1,
+    type: ['grass', 'poison'],
     },
     {
-      name: 'Venusaur',
-      height: 2,
-      type: ['grass', 'poison'],
+    name: 'Venusaur',
+    height: 2,
+    type: ['grass', 'poison'],
     },
-  ];
+];
 
-  function getAll() {
-    return pokemonList;
-  }
-
-  function add(pokemon) {
+function add(pokemon) {
+    if (
+    typeof pokemon === 'object' && 
+    'name' in pokemon && 
+    'height' in pokemon &&
+    'types' in pokemon) 
+    {
     pokemonList.push(pokemon);
-  }
+    } else {
+    console.log('pokemon is not correct');
+    }
+}
 
-  return {
+function getAll() {
+    return pokemonList;
+}
+
+function addListItem (pokemon) {
+    // assigns the list of pokemons to HTML
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+
+    // adds class to button for CSS styling
+    button.classList.add('button-class');
+
+    // append children
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+
+    // add event listener
+    button.addEventListener('click', function () {
+        showDetails(pokemon)
+    });
+}
+
+function showDetails (pokemon) {
+    console.log(pokemon);
+}
+
+return {
     getAll: getAll,
     add: add,
-  };
+    addListItem: addListItem
+};
 })();
 
-//prints pokemon list in index page
+console.log(pokemonRepository.getAll(
+));
+
 pokemonRepository.getAll().forEach(function (pokemon) {
-  document.write(
-    `<p>${pokemon.name}<br>Height: ${pokemon.height}m<br>Type: ${pokemon.type}</p>`
-  );
-});
+    pokemonRepository.addListItem(pokemon);
+}); 
